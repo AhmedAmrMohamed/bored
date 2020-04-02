f556408fda4ed5aa7ef9974157745ac7ad6c3bcc
Ahmed Amr <ahmedbonumstelio@gmail.com>
2020-04-02 03:35:44 +0200

    added memoization for speed

--- editdis.py
+++ editdis.py
@@ -13,27 +13,33 @@ class EditDis:
     def dist(self,firstLine,secondLine):
         firstLine,secondLine = self.proc(firstLine),self.proc(secondLine)
         firstlen,secondlen = len(firstLine),len(secondLine)
+        meme = {}
         def core(fidx, sidx):
+            ret = meme.get((fidx,sidx),None)
+            if ret:
+                return ret
             if fidx == firstlen:
                 return self.insert * (secondlen - sidx)
             if sidx == secondlen:
                 return self.remove * (firstlen  - fidx)
 
             if firstLine[fidx] == secondLine[sidx]:
-                return core(fidx+1,sidx+1)
+                return core(fidx+1,sidx+1)-1
 
             add = core(fidx+1,sidx  ) + self.remove
             rem = core(fidx  ,sidx+1) + self.insert
             cha = core(fidx+1,sidx+1) + self.change
-            return min(add,rem,cha)
+            ret =  min(add,rem,cha)
+            meme[(fidx,sidx)] = ret
+            return ret
         return core(0,0)
 
 
-import sys
-a = sys.argv[1:]
-a = ' '.join(a)
-a = a.split(',')
-x = EditDis(1,2,2) 
-print(a[0],a[1],x.dist(a[0],a[1]))
+# import sys
+# a = sys.argv[1:]
+# a = ' '.join(a)
+# a = a.split(',')
+# x = EditDis(1,2,2) 
+# print(a[0],a[1],x.dist(a[0],a[1]))
                 
 
