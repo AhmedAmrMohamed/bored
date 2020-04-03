4c73bdba9368f39428f436ca9d577fe23d2c91de
Ahmed Amr <ahmedbonumstelio@gmail.com>
2020-04-03 22:56:51 +0200

    conf file added

--- cli.py
+++ cli.py
@@ -1,10 +1,17 @@
 import os
+import json
 import control
 def main():
+    conf = getConf()
+    ch = conf['change']
+    ad = conf['insert']
+    re = conf['remove']
+    ma = conf['acceptableMatchDgree']
+    topres = conf['topres']
     target = input("the phrase : ")
     print('Enter the subs folder or press Enter\nTo use the current directory:\n\
     {}'.format(os.getcwd()))
-    path = input()
+    path = input('Path/Entre : ')
     if len(path) == 0:
         path = None
     else:
@@ -14,11 +21,17 @@ def main():
             print('directory not found')
             quit
     print('please hold ...')
-    res =  control.Control(target,path)
-    mat = min(5,len(res.res))
+    res =  control.Control(target,path,change = ch,remove = re,insert=ad,matchdegree = ma)
+    mat = min(topres,len(res.res))
     print(f'here are the top {mat} matches')
     while mat:
         mat-=1
         print(res.res.pop())
+
+def getConf():
+    with open('conf.txt','r') as f:
+        r = f.read()
+        return json.loads(r)
+
 
 main()
