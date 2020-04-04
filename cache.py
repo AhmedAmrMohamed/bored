32755c4463dfd4c39f707a8973c84a755352cac3
Ahmed Amr <ahmedbonumstelio@gmail.com>
2020-04-04 05:46:16 +0200

    simple cache approach

---
+++ cache.py
@@ -0,0 +1,54 @@
+import os
+import pickle
+class Cache:
+    def __init__(self):
+        self.cache = {}      #cache['filenodenumber'] = {(lastmodified, lsta)}
+    
+    def get(self,file):
+        ''' 
+        return None if file can't be loaded from
+        cache, otherwise
+        return cached values
+        '''
+        ino,mtime =  self.getstats(file)
+        cached    =  self.cache.get(ino,None)
+        if not cached  or cached[0] != mtime:
+            return False
+        return cached[1]
+    
+    def getstats(self,filepath):
+        ''' return filenodenumber, lastmodifyingtime '''
+        st = os.stat(filepath)
+        return st.st_ino, st.st_mtime
+        
+
+    def put(self,file,val):
+        ''' put into cache'''
+        ino,mtime = self.getstat(file)
+        self.cache[ino] = (mtime,val)
+        return val
+
+    def mange(self,file):
+        status = os.stat(file)
+        status = status.st_ino, status.st_mtime
+        return self.get(status)
+
+
+    def load(self):
+        ''' load the cache from file '''
+        f = open('cache.bn','rb')
+        self.cache = pickle.load(f)
+        f.close()
+    
+    def dump(self):
+        ''' write the cache to a file '''
+        f = open('cache.bn','wb')
+        pickle.dump(self.cache,f)
+        f.close()
+
+# import time
+# a,b = x.getstats('extra/gen.srt')
+# x.put(a,b,'smt')
+# print(time.time())
+# print(x.getstats('cache.py'))
+
